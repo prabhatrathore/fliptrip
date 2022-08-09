70f18449f13577954340f8a98f4fbadd41c440e3
Bhaskar Pandey <43948993+s7k11@users.noreply.github.com>
2022-08-09 10:15:33 +0530

    changes

--- src/initialpage/App.jsx
+++ src/initialpage/App.jsx
@@ -1,26 +1,24 @@
-import React, { Component, useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { Redirect, Route, Switch } from "react-router-dom";
-// We will create these two pages in a moment
-//Authendication
-import LoginPage from "./loginpage";
-import RegistrationPage from "./RegistrationPage";
-import ForgotPassword from "./forgotpassword";
-import OTP from "./otp";
-import LockScreen from "./lockscreen";
-import ApplyJobs from "./ApplyJob";
-
-//Main App
-import DefaultLayout from "./Sidebar/DefaultLayout";
-import Settinglayout from "./Sidebar/Settinglayout";
-import Tasklayout from "./Sidebar/tasklayout";
-import Emaillayout from "./Sidebar/emaillayout";
-import chatlayout from "./Sidebar/chatlayout";
-import { useDispatch, useSelector } from "react-redux";
-
-import uicomponents from "../MainPage/UIinterface/components";
 //Error Page
 import Error404 from "../MainPage/Pages/ErrorPage/error404";
 import Error500 from "../MainPage/Pages/ErrorPage/error500";
+import uicomponents from "../MainPage/UIinterface/components";
+import ApplyJobs from "./ApplyJob";
+import ForgotPassword from "./forgotpassword";
+import LockScreen from "./lockscreen";
+// We will create these two pages in a moment
+//Authendication
+import LoginPage from "./loginpage";
+import OTP from "./otp";
+import RegistrationPage from "./RegistrationPage";
+import chatlayout from "./Sidebar/chatlayout";
+//Main App
+import DefaultLayout from "./Sidebar/DefaultLayout";
+import Emaillayout from "./Sidebar/emaillayout";
+import Settinglayout from "./Sidebar/Settinglayout";
+import Tasklayout from "./Sidebar/tasklayout";
 
 // {
 //     if (location.pathname.includes("login") || location.pathname.includes("register") || location.pathname.includes("forgotpassword") || location.pathname.includes("otp") || location.pathname.includes("lockscreen")) {
